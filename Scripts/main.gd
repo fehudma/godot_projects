1ffe8fb8dbbf232f7e830720d5a4cbc46a76c099
fehu.dma <fehu.dma@gmail.com>
2026-07-20 13:02:09 +0300

    save button writes data into file

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -118,7 +118,11 @@ func _on_upgrade_button_pressed() -> void:
 
 func _on_save_button_pressed() -> void:
 	var save_data: Dictionary = collect_save_data()
-	print(save_data)
+	var json_text: String = JSON.stringify(save_data)
+	var save_file: FileAccess = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	save_file.store_string(json_text)
+	print("save_data: " + str(save_data))
+	print("json_text: " + str(json_text))
 
 func _on_load_button_pressed() -> void:
 	pass # Replace with function body.
