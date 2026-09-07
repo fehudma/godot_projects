05e7a68d870c31b74e8364bfce9419b5f6f20076
fehu.dma <fehu.dma@gmail.com>
2026-09-07 17:43:31 +0300

    Set level when generating a new character

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -18,6 +18,7 @@ const MAX_HEALTH: int = 40
 #=======================VARs
 var character: Dictionary = {
 	"name": "Unknown",
+	"level": 1,
 	"attack": 0,
 	"defense": 0,
 	"health": 0
@@ -27,9 +28,11 @@ var has_character: bool = false
 #=======================OnReadies
 @onready var recruit_button: Button = $RecruitButton
 @onready var name_label: Label = $VBoxContainer/NameLabel
+@onready var level_label: Label = $VBoxContainer/LevelLabel
 @onready var health_label: Label = $VBoxContainer/HealthLabel
 @onready var attack_label: Label = $VBoxContainer/AttackLabel
 @onready var defense_label: Label = $VBoxContainer/DefenseLabel
+
 
 
 
@@ -41,12 +44,14 @@ func update_stats_display() -> void:
 		attack_label.text = "Attack: -"
 		defense_label.text = "Defense: -"
 		health_label.text = "Health: -"
+		level_label.text = "Level: " + str(character["level"])
 		return
 	
 	name_label.text = "Name: " + str(character["name"]) 
 	attack_label.text = "Attack: " + str(character["attack"])
 	defense_label.text = "Defense: " + str(character["defense"])
 	health_label.text = "Health: " + str(character["health"])
+	level_label.text = "Level: " + str(character["level"])
 
 #character generation
 func generate_character() -> void:
