7f1becd47c43bc9153133c3e7e2e009fff6d1085
fehu.dma <fehu.dma@gmail.com>
2026-09-07 14:49:48 +0300

    simple random functions for attack, defense and name

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,4 +1,27 @@
 extends Control
+#=======================VARs
+var attack: int = 10
+var defense: int = 5
 
+#=======================OnReadies
+@onready var attack_label: Label = $AttackLabel
+
+#=======================HELPER FUNCTIONs
+func update_stats_display() -> void:
+	$AttackLabel.text = "Attack: " + str(attack)
+	$DefenseLabel.text = "Defense: " + str(defense)
+
+#=======================INIT
+
+func _ready() -> void:
+	update_stats_display()
+
+
+
+
+
+#=======================SIGNALs
 func _on_recruit_button_pressed() -> void:
-	print("Recruit button pressed")
+	attack = randi_range(5, 15)
+	defense = randi_range(3, 10)
+	update_stats_display()
